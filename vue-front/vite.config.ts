42a2b8b3a15a8be35249a85a0c3abdbf1c1d4afd
satter0827 <brts50nw8q@gmail.com>
2024-07-13 21:35:48 +0900

    vuetifyの取り込み

--- vue-front/vite.config.ts
+++ vue-front/vite.config.ts
@@ -5,6 +5,9 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
+import vuetify from 'vite-plugin-vuetify'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -12,9 +15,15 @@ export default defineConfig({
     vueJsx(),
     vueDevTools(),
   ],
+  server: {
+    host: true
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+    build: {
+      sourcemap: true,
   }
 })
